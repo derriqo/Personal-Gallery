74fed102e48a88eb877f593bef9fcbec41fce713
derrrick william <williamderrick100@gmail.com>
2019-03-01 13:53:19 +0300

    Added models.

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    picha_name = models.CharField(max_length =30)
+    description = models.CharField(max_length =100)
+
+class Location(models.Model):
+    place = models.CharField(max_length =30)
+
+class Category(models.Model):
+    cat_name = models.CharField(max_length =30)
+    
