ddc442350d96e2d3935414c8f069fdd8e0496fdb
derrrick william <williamderrick100@gmail.com>
2019-03-05 07:14:03 +0300

    Made necessary changes for deployment process.

--- art/settings.py
+++ art/settings.py
@@ -51,7 +51,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&2m=4p+*&!+8e!ipo@jpxa4*6k*%q(qp-!rt+=-i@qjsa97@6s'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
