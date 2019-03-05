0e73cab5676cb21365ca2837152448111ebc45e4
derrrick william <williamderrick100@gmail.com>
2019-03-05 04:55:58 +0300

    added first test

--- gallery/tests.py
+++ gallery/tests.py
@@ -4,4 +4,4 @@ from .models import Image,Location,Category
 # Create your tests here.
 class ImageTestClass(TestCase):
     def setUp(self):
-        self.
+        self.image = Image(image = 'derro',image_name = 'beach',description = 'along the beach',location = 'kwale',category = 'sport')
